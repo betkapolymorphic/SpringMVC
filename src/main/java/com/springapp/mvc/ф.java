d08b86f71f231337439c47f84548ec5a2afd5df0
Beta <multirespect1@gmail.com>
2014-08-10 15:37:58 +0300

    Working registration

--- src/main/java/com/springapp/mvc/ф.java
+++ src/main/java/com/springapp/mvc/ф.java
@@ -1,21 +1,31 @@
 package com.springapp.mvc;
 
-import com.springapp.mvc.Model.UserAuthInfo;
-import com.springapp.mvc.Model.UserAuthRole;
-import org.hibernate.Session;
+import java.util.HashMap;
+import java.util.*;
 
-/**
- * Created by Beta on 8/7/14.
- */
-public class ф {
+class A {
+    public String name;
+
+    public int hashCode(){
+        return name.hashCode();
+    }
+
+    public boolean equals(Object o){
+        if(o instanceof A){
+            return name.equals( ((A)o).name );
+        }
+        return false;
+    }
+
+    public static void main(int... args) {
+        System.out.println(1);
+    }
     public static void main(String[] args) {
-        Session session = HibernateUtil.getSessionFactory().
-                openSession();
-        session.beginTransaction();
-        UserAuthInfo info = new UserAuthInfo("Alber","qwe",1);
-        UserAuthRole role = new UserAuthRole("ROLE_USER",info);
-        session.save(role);
-        session.getTransaction().commit();
-        session.close();
+        int n = 3, k = 5;
+        while (n!=k) {
+            n = (n*2) % 11;
+            k = (k*7) %11;
+        }
+        System.out.println(0x2D);
     }
 }
